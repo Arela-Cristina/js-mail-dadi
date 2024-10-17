2f17e54b7ecdbb020aaf163115103e4b101cddf7
Arela-Cristina <instagrandine@gmail.com>
2024-10-17 17:29:05 +0200

    set conditions and show in console the message which correspond

--- dadi/dadi.js
+++ dadi/dadi.js
@@ -8,6 +8,15 @@ let numUser = parseInt(prompt('Scrivi un numero dal 1 al 6'));
 let numPc = Math.floor(Math.random() * 7)
 
 console.log(`numUser: ${numUser}`, `numPc: ${numPc}`)
+
 //Stabilire il vincitore, in base a chi fa il punteggio più alto.
+let messagio = '';
 //se numUser ha fatto il punteggio piu alto messagio: Hai vinto tu
-//se numPc ha fatto il punteggio piu alto messagio: Ha visto il computer
+if (numUser > numPc) {
+    messagio = 'Hai vinto tu, complimenti!';
+} else {
+    //se numPc ha fatto il punteggio piu alto messagio: Ha visto il computer
+    messagio = 'Ha vinto il computer :('
+}
+
+console.log(messagio)
