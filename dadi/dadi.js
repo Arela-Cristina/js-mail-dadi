43f9e126f28c504569916ba998315cf88c22dda0
Arela-Cristina <instagrandine@gmail.com>
2024-10-17 17:25:47 +0200

    get the values ​​of the user and the computer

--- dadi/dadi.js
+++ dadi/dadi.js
@@ -1,3 +1,13 @@
 //Gioco dei dadi
 //Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
+
+//chiediamo un numero random al utente atraverso un promt
+let numUser = parseInt(prompt('Scrivi un numero dal 1 al 6'));
+
+//facciamo generare al computer un numero random dal 1 al 6
+let numPc = Math.floor(Math.random() * 7)
+
+console.log(`numUser: ${numUser}`, `numPc: ${numPc}`)
 //Stabilire il vincitore, in base a chi fa il punteggio più alto.
+//se numUser ha fatto il punteggio piu alto messagio: Hai vinto tu
+//se numPc ha fatto il punteggio piu alto messagio: Ha visto il computer
